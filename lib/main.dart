4d1e164c7f4cdaac6c0b81a0566af87e13f8bd66
MuhammadNurhaziqbinH <muhammadnurhaziqharis@yahoo.com>
2025-04-04 00:46:56 +0800

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class LoginScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min, // Centers vertically
         children: [
           FoneAppBannerLogin(),
-          LoginSection(),
+          LoginSection(), //dsadsadsadad
         ],
       ),
     );
